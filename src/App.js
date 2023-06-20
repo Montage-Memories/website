c142dc407cbcb26abbfc3b0741d8eb378c028e61
Sandhya_Murugan <sandhya@mallow-tech.com>
2023-06-21 03:23:15 +0530

    url changes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Layout>
               <Content>
                   <Routes>
-                      <Route path="/" element={<Home />} />
+                      <Route exact path="/" element={<Home />} />
                       {/*<Route path="/about" element={<About />} />*/}
                       {/*<Route path="/contact" element={<Contact />} />*/}
                       {/*<Route path="/works" element={<Works />} />*/}
