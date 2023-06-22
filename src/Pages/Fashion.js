6bd71ec6c51caaf60813eb136acec8d90c8f4f9e
Sandhya_Murugan <sandhya@mallow-tech.com>
2023-06-22 22:13:51 +0530

    changes

--- src/Pages/Fashion.js
+++ src/Pages/Fashion.js
@@ -8,14 +8,21 @@ const Fashion = () => {
     const [selectedImage, setSelectedImage] = useState('');
 
     const images = [
-        img,
-        img,
-        img,
-        img,
-        img,
-        img,
-        img,
-        img,
+        "https://drive.google.com/uc?id=1c-YGujj4RK6DzyXDkgUWsxjH8iG6ZFM5",
+        "https://drive.google.com/uc?id=1OX_ZIyie_PchZu8nv-EKv-B5OEkl68s4",
+        "https://drive.google.com/uc?id=1SKXlWC11LVma1DGdCnbM83gfwLr6b0N8",
+        "https://drive.google.com/uc?id=1iW2x915adn2OAwi_cIWe6Mr8u7cBstgx",
+        "https://drive.google.com/uc?id=1b5omGa5hPB6WSFWje8CgrVmcKpgw3kzP",
+        "https://drive.google.com/uc?id=1lYq_CbO8xBUFPndOuT9ryE5dp8cV4n-g",
+        "https://drive.google.com/uc?id=1Rl7m90-MXnu_jjjysSLiiMltvwl9lNAj",
+        "https://drive.google.com/uc?id=1scgmpuYt5AFig-lmVql2_H5b9gZUVDz7",
+        "https://drive.google.com/uc?id=1BxUEhkHdGUs7XI_4HpInFBqP1DPAUlhA",
+        "https://drive.google.com/uc?id=1kZnrM2vmh1uvtcvoCqCIPqZlY2XA3p2X",
+        "https://drive.google.com/uc?id=1Og51jKOwxOBuv8Zr6P2YMJ_ZuyxYSAyl",
+        "https://drive.google.com/uc?id=1SkrL7xIZugnHcODbmVyyE4y-Go3_ysjg",
+        "https://drive.google.com/uc?id=1XBT8xiZ5sAccs-DX8Wp8xf4pnwPtRwh-",
+        "https://drive.google.com/uc?id=1GBjPnjtGuzPxhsadHleYeA3ZAVI8XsLg",
+        "https://drive.google.com/uc?id=1c-YGujj4RK6DzyXDkgUWsxjH8iG6ZFM5",
     ];
 
     const handleImageClick = (image) => {
@@ -54,7 +61,7 @@ const Fashion = () => {
                                 <img
                                     src={image}
                                     alt={`Image ${index + 1}`}
-                                    style={{ width: '100%', height: 'auto' }}
+                                    style={{ width: '100%', height: '300px' }}
                                 />
                             </div>
                         </Col>
