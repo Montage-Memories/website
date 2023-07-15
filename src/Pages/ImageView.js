192abf4812a6857504186893973cd1c99a999268
Sandhya_Murugan <sandhya@mallow-tech.com>
2023-07-15 20:25:45 +0530

    updates

--- src/Pages/ImageView.js
+++ src/Pages/ImageView.js
@@ -106,7 +106,7 @@ const ImageView = () => {
         selectedDataset = dataset14;
     }
     return (
-        <div style={{margin:"auto",padding:"2%"}}>
+        <div style={{margin:"auto",padding:"2%", backgroundColor:"white"}}>
                 <ResponsiveMasonry columnsCountBreakPoints={{350:1,750:2, 900:3}}>
                     <Masonry columnsCount={3} gutter="0px">
                         {selectedDataset.map((item) => (
